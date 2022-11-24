f02895350c887948d1ba42566a8d97046f5ee593
Vadim Nechaev <nech.vadim.eug@gmail.com>
2022-11-24 12:20:32 +0300

    Added 'Extra symbols' and '6 types of data' checks.

--- tempFunctions.c
+++ tempFunctions.c
@@ -4,31 +4,36 @@
 
 void readFile(FILE* f){
     int year, month, day, hour, minute, temp;
-    int line = 1;
-    char buffer[30] = "*****************************"; // Fill array with '*'
+    int line = 1, symbolsCount = -1;
+    char buffer[21] = "*********************"; // Fill array with '*'
+    char ch;
 
-    for (line = 1; fscanf(f, "%[^\n]s", buffer) != EOF; line++) // Loop by strings
+    for (line = 1; fscanf(f, "%20[^\n]s", buffer) != EOF; line++) // Loop by lines
     {
-        while (fgetc(f) != '\n' && fgetc(f) != EOF); // "Eating" '\n' or 'EOF'
+        /* Extra symbols check */
+        do {
+            ch = fgetc(f);
+            symbolsCount++;
+        } while (ch != '\n' && ch != EOF);
+        
+        if (symbolsCount > 0) {
+            printf("Error in line %2d\n", line);
+            symbolsCount = -1;
+            continue;
+        }
+        symbolsCount = -1;
 
-        /* Debug buffer out */
-        printf("Line %2d:   ", line);
-        printf("%s", buffer);
-        printf("\n");
+        /* Fill data in variables */    
+        if (sscanf(buffer, "%d%*c%d%*c%d%*c%d%*c%d%*c%d",   // Read numbers and any symbol between
+                            &year, &month, &day, &hour, &minute, &temp) != paramNumbers){   // 6 types of data check
+            printf("Error in line %2d\n", line);
+        }
+        else {
+            // printf ("Line %2d +\n", line);
+            printf("Line %2d >>>  year:%4d\tmonth:%2d\tday:%2d\thour:%2d\tminute:%2d\ttemp:%3d\n", line, year, month, day, hour, minute, temp);
+        }
 
-        strcpy(buffer, "*****************************"); // Fill array with '*' 
+    
+
     }
-    
-    
-
-    
-    // printf(">>> %d\n", strlen(buffer));   // +20
-
-    // if (sscanf(buffer, "%d;%d;%d;%d;%d;%d;", &year, &month, &day, &hour, &minute, &temp) != paramNumbers){
-    //     printf("Fields ERROR!\n");
-    // }
-    // else
-    //     printf("Great!\n");
-
-    // printf("%d\n", year + 100);
 }
