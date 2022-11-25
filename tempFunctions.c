62dd3979a908197440bfaffd38d05d38f09aa706
Vadim Nechaev <nech.vadim.eug@gmail.com>
2022-11-25 12:59:58 +0300

    Added correct data check function.

--- tempFunctions.c
+++ tempFunctions.c
@@ -2,6 +2,58 @@
 
 #define paramNumbers 6
 
+bool correctData_check(int y, int mon, int d, int h, int min, int t){
+    bool error = false;
+    
+    /* Leap year CHECK */
+    if (y % 4 == 0 && mon == 2 && d > 29)       error = true;   // if leap year and > 29 days in febr
+    if (y % 4 != 0 && mon == 2 && d > 28)       error = true;   // if not leap year and > 28 days in febr
+
+    /* Month count CHECK */
+    if (mon < 1 || mon > 12)                    error = true;
+
+    /* Hours CHECK */
+    if (h < 0 || h > 23)                        error = true;
+
+    /* Minutes CHECK */
+    if (min < 0 || min > 59)                    error = true;
+
+    /* Temperature CHECK */
+    if (t < -99 || t > 99)                      error = true;
+
+    /* Days in months CHECK */
+    switch (mon)
+    {
+    case 1:     // Jun
+        if (d < 0 || d > 31)    error = true;   break;
+    case 3:     // March
+        if (d < 0 || d > 31)    error = true;   break;
+    case 4:     // Apr
+        if (d < 0 || d > 30)    error = true;   break;
+    case 5:     // May
+        if (d < 0 || d > 31)    error = true;   break;
+    case 6:     // Jun
+        if (d < 0 || d > 30)    error = true;   break;
+    case 7:     // July
+        if (d < 0 || d > 31)    error = true;   break;
+    case 8:     // Aug
+        if (d < 0 || d > 31)    error = true;   break;
+    case 9:     // Sep
+        if (d < 0 || d > 30)    error = true;   break;
+    case 10:    // Oct
+        if (d < 0 || d > 31)    error = true;   break;
+    case 11:    // Nov
+        if (d < 0 || d > 30)    error = true;   break;
+    case 12:    // Dec
+        if (d < 0 || d > 31)    error = true;   break;
+    default:
+        if (d < 0 || d > 31)    error = true;   break;
+    }
+
+    return error;
+}
+
+
 void readFile(FILE* f){
     int year, month, day, hour, minute, temp;
     int line = 1, symbolsCount = -1;
@@ -30,7 +82,12 @@ void readFile(FILE* f){
         }
         else {
             // printf ("Line %2d +\n", line);
-            printf("Line %2d >>>  year:%4d\tmonth:%2d\tday:%2d\thour:%2d\tminute:%2d\ttemp:%3d\n", line, year, month, day, hour, minute, temp);
+            if (correctData_check(year, month, day, hour, minute, temp)) {
+                printf("Error in line %2d\n", line);
+            }
+            else {
+                printf("Line %2d >>>  year:%4d\tmonth:%2d\tday:%2d\thour:%2d\tminute:%2d\ttemp:%3d\n", line, year, month, day, hour, minute, temp);
+            }
         }
 
     
--- tempFunctions.h
+++ tempFunctions.h
@@ -3,9 +3,10 @@
 #define TEMPFUNCTIONS_H
 #include <stdio.h>
 #include <string.h>
+#include <stdbool.h>
 
 
 void readFile(FILE* f);
-
+bool correctData_check(int y, int mon, int d, int h, int min, int t);
 
 #endif
